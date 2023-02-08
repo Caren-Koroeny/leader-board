f28b4887377f9503446bff1d79a8865b95bac5f3
Caren-Koroeny <carensiya5@gmail.com>
2023-02-08 17:07:20 +0300

    impemented api functionalities

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import './style.css';
+import { postScores, getScores} from '../modules/createGame';
+
+const refreshButton = document.getElementById("refresh-btn");
+const listCont = document.getElementById("listcont");
+const form = document.getElementById('form');
+const name = document.getElementById("userName");
+const scores = document.getElementById("Score");
+
+
+
+// GET Request
+const update = async() => {
+    const display = await getScores()
+    listCont.innerHTML = "";
+    console.log("games", display)
+    display.forEach(element => {
+        const item = document.createElement("li");
+        item.innerHTML = `
+            ${element.user} : ${element.score}
+        `
+        listCont.appendChild(item)
+        
+
+        
+    });
+}
+
+refreshButton.addEventListener('click', update)
+
+// POST REQUEST
+
+form.addEventListener('submit', (e) =>{
+    const formData = {
+        user:name.value,
+        score:scores.value
+    }
+    e.preventDefault();
+    postScores(formData);
+    name.value ='';
+    scores.value = '';
+})
+
